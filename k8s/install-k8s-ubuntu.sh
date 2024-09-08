10ae3da9a86f87494de010e4c9d22f42d6d00d2b
jroosing <koari4k@gmail.com>
2024-09-08 11:35:26 +0200

    change k8s repo

--- k8s/install-k8s-ubuntu.sh
+++ k8s/install-k8s-ubuntu.sh
@@ -16,8 +16,8 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-relea
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/kubernetes-xenial main"
 
 # Update package index
 sudo apt-get update
